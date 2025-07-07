e7c4986e76bd32dc62b23126b89a20cfff512585
Douglas-Nkubitu <nkubitudouglas@gmail.com>
2025-07-07 16:15:59 +0300

    feat: Add custom item management features

---
+++ mbquarry/mbquarry/doctype/item_location/item_location.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, Team Web Africa and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Item Location", {
+// 	refresh(frm) {
+
+// 	},
+// });
